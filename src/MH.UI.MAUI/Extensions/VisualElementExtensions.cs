0d00f7da1661674dfdd26f70e00d2dd216b4532b
martin-holy <martin.holy@hotmail.com>
2025-05-09 14:13:59 +0200

    VisualElementExtensions: FindAncestorOfType

---
+++ src/MH.UI.MAUI/Extensions/VisualElementExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MH.UI.MAUI.Extensions;
+
+public static class VisualElementExtensions {
+  public static T? FindAncestorOfType<T>(this VisualElement? element) where T : VisualElement {
+    while (element != null) {
+      if (element is T ancestor) return ancestor;
+      element = element.Parent as VisualElement;
+    }
+
+    return null;
+  }
+}
