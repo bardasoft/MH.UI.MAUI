ad000c36eca80186b829f7a26fdeff37ba2fcf63
martin-holy <martin.holy@hotmail.com>
2025-05-09 13:59:49 +0200

    ThicknessExtensions: FromThicknessD

---
+++ src/MH.UI.MAUI/Extensions/ThicknessExtensions.cs
@@ -0,0 +1,9 @@
+﻿using MH.Utils.Types;
+using Microsoft.Maui;
+
+namespace MH.UI.MAUI.Extensions;
+
+public static class ThicknessExtensions {
+  public static Thickness FromThicknessD(this ThicknessD thickness) =>
+    new(thickness.Left, thickness.Top, thickness.Right, thickness.Bottom);
+}
