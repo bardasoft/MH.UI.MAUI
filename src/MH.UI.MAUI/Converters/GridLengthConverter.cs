feb06c590288d7ba3d1dedc20893a2886663582b
martin-holy <martin.holy@hotmail.com>
2025-05-10 12:40:36 +0200

    GridLengthConverter

---
+++ src/MH.UI.MAUI/Converters/GridLengthConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui;
+
+namespace MH.UI.MAUI.Converters;
+
+public class GridLengthConverter : BaseConverter {
+  private static readonly object _lock = new();
+  private static GridLengthConverter? _inst;
+  public static GridLengthConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
+
+  public override object Convert(object? value, object? parameter) =>
+    value switch {
+      int i => new(i),
+      double d => new(d),
+      _ => GridLength.Auto
+    };
+
+  public override object? ConvertBack(object? value, object? parameter) =>
+    ((GridLength?)value)?.Value;
+}
