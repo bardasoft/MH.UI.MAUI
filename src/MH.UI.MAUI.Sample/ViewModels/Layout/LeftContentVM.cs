a84237297aa48064a716fb716918349469c8626f
martin-holy <martin.holy@hotmail.com>
2025-05-11 20:08:22 +0200

    Sample app: LeftContentVM

---
+++ src/MH.UI.MAUI.Sample/ViewModels/Layout/LeftContentVM.cs
@@ -0,0 +1,9 @@
+﻿using MH.UI.Controls;
+
+namespace MH.UI.MAUI.Sample.ViewModels.Layout;
+
+public class LeftContentVM : TabControl {
+  public LeftContentVM() : base(new(Dock.Left, Dock.Top, new SlidePanelPinButton()) { JustifyTabSize = true }) {
+    CanCloseTabs = true;
+  }
+}
