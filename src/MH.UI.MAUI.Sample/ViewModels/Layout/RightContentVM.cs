157efdbe34396921d8a037a9a5ea8b02e69a4da8
martin-holy <martin.holy@hotmail.com>
2025-05-13 08:00:26 +0200

    RightContentVM

---
+++ src/MH.UI.MAUI.Sample/ViewModels/Layout/RightContentVM.cs
@@ -0,0 +1,19 @@
+﻿//using MH.UI.MAUI.Sample.Models;
+using MH.UI.Controls;
+//using MH.UI.MAUI.Sample.ViewModels.Controls;
+using MH.Utils.BaseClasses;
+
+namespace MH.UI.MAUI.Sample.ViewModels.Layout;
+
+// TODO PORT
+public class RightContentVM : ObservableObject {
+  /*private FolderM? _selectedFolder;
+
+  public FolderM? SelectedFolder { get => _selectedFolder; private set { _selectedFolder = value; OnPropertyChanged(); } }
+  public FolderTreeViewVM FolderTreeView { get; } = new();*/
+  public SlidePanelPinButton SlidePanelPinButton { get; } = new();
+
+  public RightContentVM() {
+    //FolderTreeView.ItemSelectedEvent += (_, item) => SelectedFolder = item as FolderM;
+  }
+}
