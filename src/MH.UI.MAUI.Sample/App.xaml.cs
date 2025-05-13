6d5cfe76fae537d9f61fc52e710e8133e02e0963
martin-holy <martin.holy@hotmail.com>
2025-05-13 08:04:41 +0200

    Sample app: app

--- src/MH.UI.MAUI.Sample/App.xaml.cs
+++ src/MH.UI.MAUI.Sample/App.xaml.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.Maui.Controls;
+﻿using MH.UI.MAUI.Sample.Views;
+using System.Threading.Tasks;
 
 namespace MH.UI.MAUI.Sample;
 
-public partial class App : Application {
+public partial class App {
+  public static Core Core { get; private set; } = null!;
+  public static CoreUI CoreUI { get; private set; } = null!;
+
   public App() {
     InitializeComponent();
 
-    MainPage = new AppShell();
+    // TODO PORT
+    //MH.UI.MAUI.Utils.ColorHelper.AddColorsToResources();
+
+    // TODO PORT
+    var splashScreen = new SplashScreenV();
+    var mainPage = new MainPage();
+    MainPage = mainPage;
+
+    Task.Run(() => {
+      Core.Inst.InitAsync(splashScreen.ProgressMessage).ContinueWith(_ => {
+        Core = Core.Inst;
+        CoreUI = new();
+        Core.AfterInit();
+        mainPage.BindingContext = Core.VM.MainWindow;
+        //MainPage = new MainPage(); // changing MainPage doesn't work well
+      });
+    });
   }
 }
