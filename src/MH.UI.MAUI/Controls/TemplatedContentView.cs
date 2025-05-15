b050a4b7b83f1d39676247c93de0483a620502ed
martin-holy <martin.holy@hotmail.com>
2025-05-15 10:09:53 +0200

    TemplatedContentView

---
+++ src/MH.UI.MAUI/Controls/TemplatedContentView.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MH.UI.MAUI.Controls;
+
+public class TemplatedContentView : ContentView {
+  private bool _firstBindingContextChange = true;
+  private bool _wasContentSet;
+
+  public static DataTemplateSelector? FallbackDataTemplateSelector { get; set; }
+
+  protected override void OnBindingContextChanged() {
+    base.OnBindingContextChanged();
+
+    if (_firstBindingContextChange) {
+      _firstBindingContextChange = false;
+      if (Content != null) _wasContentSet = true;
+    }
+
+    if (_wasContentSet) return;
+
+    Content = BindingContext == null && !_wasContentSet
+      ? null
+      : FallbackDataTemplateSelector?.SelectTemplate(BindingContext, this).CreateContent() as View;
+  }
+}
