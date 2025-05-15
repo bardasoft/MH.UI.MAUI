83966f9910f42960a4ef57214e3937e4c60184e9
martin-holy <martin.holy@hotmail.com>
2025-05-15 11:05:36 +0200

    Sample app: FallbackDataTemplateSelector

--- src/MH.UI.MAUI.Sample/CoreUI.cs
+++ src/MH.UI.MAUI.Sample/CoreUI.cs
@@ -1,7 +1,17 @@
-﻿namespace MH.UI.MAUI.Sample;
+﻿using MH.UI.MAUI.Controls;
+using MH.UI.MAUI.Sample.ViewModels.Layout;
+using MH.Utils.Interfaces;
+
+namespace MH.UI.MAUI.Sample;
 
 public class CoreUI {
   public CoreUI() {
+    TemplatedContentView.FallbackDataTemplateSelector =
+      new TypeDataTemplateSelector([
+        new(typeof(IListItem), "MH.DT.IListItem"),
+        new(typeof(MiddleContentVM), "S.DT.Views.Layout.MiddleContentV")
+      ]);
+
     // TODO PORT
   }
 }
