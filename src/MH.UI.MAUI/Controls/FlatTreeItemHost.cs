b7225f1eb2206848e5d25bd1acd9cb3acd553234
martin-holy <martin.holy@hotmail.com>
2025-05-16 14:52:28 +0200

    FlatTreeItemHost

---
+++ src/MH.UI.MAUI/Controls/FlatTreeItemHost.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MH.UI.MAUI.Controls;
+
+public class FlatTreeItemHost : ContentView {
+  public static readonly BindableProperty InnerContentTemplateProperty =
+    BindableProperty.Create(nameof(InnerContentTemplate), typeof(DataTemplate), typeof(FlatTreeItemHost));
+
+  public DataTemplate InnerContentTemplate {
+    get => (DataTemplate)GetValue(InnerContentTemplateProperty);
+    set => SetValue(InnerContentTemplateProperty, value);
+  }
+}
