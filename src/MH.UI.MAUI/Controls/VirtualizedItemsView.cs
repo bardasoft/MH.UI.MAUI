e33747892c78834c208291f892300b08e4f34325
martin-holy <martin.holy@hotmail.com>
2025-05-18 14:55:31 +0200

    VirtualizedItemsView

--- src/MH.UI.MAUI/Controls/VirtualizedItemsView.cs
+++ src/MH.UI.MAUI/Controls/VirtualizedItemsView.cs
@@ -1,14 +1,22 @@
-﻿using Microsoft.Maui.Controls;
-using System.Collections.Generic;
+﻿using System.Collections;
+using Microsoft.Maui.Controls;
 
 namespace MH.UI.MAUI.Controls;
 
 public class VirtualizedItemsView : View {
-  public static readonly BindableProperty ItemsSourceProperty =
-    BindableProperty.Create(nameof(ItemsSource), typeof(IEnumerable<string>), typeof(VirtualizedItemsView));
+  public static readonly BindableProperty InnerContentTemplateProperty =
+    BindableProperty.Create(nameof(InnerContentTemplate), typeof(DataTemplate), typeof(VirtualizedItemsView));
 
-  public IEnumerable<string> ItemsSource {
-    get => (IEnumerable<string>)GetValue(ItemsSourceProperty);
+  public DataTemplate? InnerContentTemplate {
+    get => (DataTemplate?)GetValue(InnerContentTemplateProperty);
+    set => SetValue(InnerContentTemplateProperty, value);
+  }
+
+  public static readonly BindableProperty ItemsSourceProperty =
+    BindableProperty.Create(nameof(ItemsSource), typeof(IEnumerable), typeof(VirtualizedItemsView));
+
+  public IEnumerable? ItemsSource {
+    get => (IEnumerable?)GetValue(ItemsSourceProperty);
     set => SetValue(ItemsSourceProperty, value);
   }
 }
