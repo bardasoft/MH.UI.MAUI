5d3e99f0ddc49b11c7b2b249cce53195b22fd1a1
martin-holy <martin.holy@hotmail.com>
2025-05-21 11:03:35 +0200

    AttachedProperties: Text

---
+++ src/MH.UI.MAUI/AttachedProperties/Text.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MH.UI.MAUI.AttachedProperties;
+
+public static class Text {
+  public static readonly BindableProperty TextProperty =
+    BindableProperty.CreateAttached("Text", typeof(string), typeof(Text), null);
+  public static readonly BindableProperty ShadowProperty =
+    BindableProperty.CreateAttached("Shadow", typeof(bool), typeof(Text), false);
+
+  public static string GetText(BindableObject view) => (string)view.GetValue(TextProperty);
+  public static void SetText(BindableObject view, string value) => view.SetValue(TextProperty, value);
+  public static bool GetShadow(BindableObject view) => (bool)view.GetValue(ShadowProperty);
+  public static void SetShadow(BindableObject view, bool value) => view.SetValue(ShadowProperty, value);
+}
