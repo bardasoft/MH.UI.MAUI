ce788551459b316fc1d303848db90bb049897097
martin-holy <martin.holy@hotmail.com>
2025-05-24 14:14:23 +0200

    Icon attached properties

--- src/MH.UI.MAUI/AttachedProperties/Icon.cs
+++ src/MH.UI.MAUI/AttachedProperties/Icon.cs
@@ -1,22 +1,22 @@
 ﻿using Microsoft.Maui.Controls;
-using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Controls.Shapes;
 
 namespace MH.UI.MAUI.AttachedProperties;
 
 public static class Icon {
   public static readonly BindableProperty DataProperty =
-    BindableProperty.CreateAttached("Data", typeof(string), typeof(Icon), null);
+    BindableProperty.CreateAttached("Data", typeof(PathGeometry), typeof(Icon), null);
   public static readonly BindableProperty FillProperty =
-    BindableProperty.CreateAttached("Fill", typeof(Color), typeof(Icon), null);
+    BindableProperty.CreateAttached("Fill", typeof(Brush), typeof(Icon), null);
   public static readonly BindableProperty ResProperty =
     BindableProperty.CreateAttached("Res", typeof(string), typeof(Icon), null);
   public static readonly BindableProperty SizeProperty =
     BindableProperty.CreateAttached("Size", typeof(double), typeof(Icon), 0.0);
 
-  public static string GetData(BindableObject view) => (string)view.GetValue(DataProperty);
-  public static void SetData(BindableObject view, string value) => view.SetValue(DataProperty, value);
-  public static Color GetFill(BindableObject view) => (Color)view.GetValue(FillProperty);
-  public static void SetFill(BindableObject view, Color value) => view.SetValue(FillProperty, value);
+  public static PathGeometry GetData(BindableObject view) => (PathGeometry)view.GetValue(DataProperty);
+  public static void SetData(BindableObject view, PathGeometry value) => view.SetValue(DataProperty, value);
+  public static Brush GetFill(BindableObject view) => (Brush)view.GetValue(FillProperty);
+  public static void SetFill(BindableObject view, Brush value) => view.SetValue(FillProperty, value);
   public static string GetRes(BindableObject view) => (string)view.GetValue(ResProperty);
   public static void SetRes(BindableObject view, string value) => view.SetValue(ResProperty, value);
   public static double GetSize(BindableObject view) => (double)view.GetValue(SizeProperty);
