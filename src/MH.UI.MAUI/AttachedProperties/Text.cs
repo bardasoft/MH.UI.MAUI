3e911092e0604779f2757ce33ac40d479d1ab113
martin-holy <martin.holy@hotmail.com>
2025-05-24 14:14:55 +0200

    Text: Color attached property

--- src/MH.UI.MAUI/AttachedProperties/Text.cs
+++ src/MH.UI.MAUI/AttachedProperties/Text.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 
 namespace MH.UI.MAUI.AttachedProperties;
 
@@ -7,9 +8,13 @@ public static class Text {
     BindableProperty.CreateAttached("Text", typeof(string), typeof(Text), null);
   public static readonly BindableProperty ShadowProperty =
     BindableProperty.CreateAttached("Shadow", typeof(bool), typeof(Text), false);
+  public static readonly BindableProperty ColorProperty =
+    BindableProperty.CreateAttached("Color", typeof(Color), typeof(Text), null);
 
-  public static string GetText(BindableObject view) => (string)view.GetValue(TextProperty);
-  public static void SetText(BindableObject view, string value) => view.SetValue(TextProperty, value);
+  public static string? GetText(BindableObject view) => (string?)view.GetValue(TextProperty);
+  public static void SetText(BindableObject view, string? value) => view.SetValue(TextProperty, value);
   public static bool GetShadow(BindableObject view) => (bool)view.GetValue(ShadowProperty);
   public static void SetShadow(BindableObject view, bool value) => view.SetValue(ShadowProperty, value);
+  public static Color? GetColor(BindableObject view) => (Color?)view.GetValue(ColorProperty);
+  public static void SetColor(BindableObject view, Color? value) => view.SetValue(ColorProperty, value);
 }
