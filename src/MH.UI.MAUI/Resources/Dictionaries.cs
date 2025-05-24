63149759d981d707948a5356d0353bf13f3f1197
martin-holy <martin.holy@hotmail.com>
2025-05-24 14:20:23 +0200

    Dictionaries: IconToBrush

---
+++ src/MH.UI.MAUI/Resources/Dictionaries.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace MH.UI.MAUI.Resources;
+
+public static class Dictionaries {
+  public static Dictionary<object, object> IconToBrush { get; set; } = new();
+}
