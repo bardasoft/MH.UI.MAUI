3b29fa1b303ba5dc7303cb2000495e15f95faecc
martin-holy <martin.holy@hotmail.com>
2025-05-29 12:28:09 +0200

    VirtualizedItemsViewHandler

--- src/MH.UI.MAUI.WinUI/Handlers/VirtualizedItemsViewHandler.cs
+++ src/MH.UI.MAUI.WinUI/Handlers/VirtualizedItemsViewHandler.cs
@@ -1,32 +1,52 @@
 ﻿using MH.UI.MAUI.Controls;
-using MH.UI.MAUI.Interfaces;
+using MH.UI.MAUI.WinUI.Controls;
 using Microsoft.Maui;
 using Microsoft.Maui.Handlers;
 using System.Collections;
 
 namespace MH.UI.MAUI.WinUI.Handlers;
 
-public class VirtualizedItemsViewHandler : ViewHandler<VirtualizedItemsView, Microsoft.UI.Xaml.Controls.ListView>, IVirtualizedItemsViewHandler {
+public class VirtualizedItemsViewHandler : ViewHandler<VirtualizedItemsView, CustomListView> {
   public static IPropertyMapper<VirtualizedItemsView, VirtualizedItemsViewHandler> PropertyMapper =
-    new PropertyMapper<VirtualizedItemsView, VirtualizedItemsViewHandler>(ViewMapper) {
-      [nameof(VirtualizedItemsView.ItemsSource)] = _mapItemsSource
-    };
+      new PropertyMapper<VirtualizedItemsView, VirtualizedItemsViewHandler>(ViewMapper) {
+        [nameof(VirtualizedItemsView.ItemsSource)] = _mapItemsSource,
+        [nameof(TreeViewHost.InnerContentTemplate)] = _mapInnerContentTemplate
+      };
 
   public static CommandMapper<VirtualizedItemsView, VirtualizedItemsViewHandler> CommandMapper = new(ViewCommandMapper);
 
   public VirtualizedItemsViewHandler() : base(PropertyMapper, CommandMapper) { }
 
-  protected override Microsoft.UI.Xaml.Controls.ListView CreatePlatformView() =>
-    new() {
-      SelectionMode = Microsoft.UI.Xaml.Controls.ListViewSelectionMode.None,
-      IsItemClickEnabled = false
-    };
+  protected override CustomListView CreatePlatformView() =>
+      new() {
+        SelectionMode = Microsoft.UI.Xaml.Controls.ListViewSelectionMode.None,
+        IsItemClickEnabled = false,
+        MauiContext = MauiContext
+      };
 
-  private static void _mapItemsSource(VirtualizedItemsViewHandler handler, VirtualizedItemsView view) {
-    handler.SetItemsSource(view.ItemsSource);
+  protected override void ConnectHandler(CustomListView platformView) {
+    base.ConnectHandler(platformView);
+    _updateItemsSource(VirtualView.ItemsSource);
   }
 
-  public void SetItemsSource(IEnumerable items) {
+  protected override void DisconnectHandler(CustomListView platformView) {
+    platformView.ItemsSource = null;
+    base.DisconnectHandler(platformView);
+  }
+
+  private static void _mapItemsSource(VirtualizedItemsViewHandler handler, VirtualizedItemsView view) =>
+    handler._updateItemsSource(view.ItemsSource);
+
+  private static void _mapInnerContentTemplate(VirtualizedItemsViewHandler handler, VirtualizedItemsView view) {
+    handler.PlatformView.InnerContentTemplate = view.InnerContentTemplate;
+  }
+
+  private void _updateItemsSource(IEnumerable? items) {
+    if (items == null) {
+      PlatformView.ItemsSource = null;
+      return;
+    }
+
     PlatformView.ItemsSource = items;
   }
 }
