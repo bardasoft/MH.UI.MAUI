80df97b0c9f21c44c0ed631d113d8baa253c3f43
martin-holy <martin.holy@hotmail.com>
2025-06-03 15:08:19 +0200

    TreeMarginConverter

---
+++ src/MH.UI.MAUI.WinUI/Converters/TreeMarginConverter.cs
@@ -0,0 +1,13 @@
+﻿using MH.Utils.BaseClasses;
+using Microsoft.UI.Xaml;
+
+namespace MH.UI.MAUI.WinUI.Converters;
+
+public partial class TreeMarginConverter : BaseConverter {
+  public override object Convert(object value, object parameter) {
+    var length = int.TryParse(parameter as string, out var l) ? l : 0;
+    var level = value is FlatTreeItem fti ? fti.Level : 0;
+
+    return new Thickness(length * level, 0.0, 0.0, 0.0);
+  }
+}
