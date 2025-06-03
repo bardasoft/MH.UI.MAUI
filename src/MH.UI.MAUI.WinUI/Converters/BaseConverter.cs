05f62e93f55545d29881703fdb6872a92d8acdee
martin-holy <martin.holy@hotmail.com>
2025-06-03 15:00:41 +0200

    BaseConverter

---
+++ src/MH.UI.MAUI.WinUI/Converters/BaseConverter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace MH.UI.MAUI.WinUI.Converters;
+
+public partial class BaseConverter : IValueConverter {
+  public virtual object? Convert(object value, Type targetType, object parameter, string language) =>
+    Convert(value, parameter);
+
+  public virtual object? Convert(object value, object parameter) =>
+    throw new NotImplementedException();
+
+  public virtual object? ConvertBack(object value, Type targetType, object parameter, string language) =>
+    ConvertBack(value, parameter);
+
+  public virtual object? ConvertBack(object value, object parameter) =>
+    throw new NotSupportedException();
+}
