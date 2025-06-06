7c8b062a8156bc5be50b2f543757b7c6e724b1cf
martin-holy <martin.holy@hotmail.com>
2025-06-06 15:30:42 +0200

    droid utils: Icons ConvertIconName

--- src/MH.UI.MAUI.Droid/Utils/Icons.cs
+++ src/MH.UI.MAUI.Droid/Utils/Icons.cs
@@ -2,17 +2,21 @@
 using Android.Graphics;
 using Android.Graphics.Drawables;
 using AndroidX.Core.Content;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MH.UI.MAUI.Droid.Utils;
 
 public static class Icons {
   public static Color DefaultColor { get; set; } = Color.Gray;
   public static Dictionary<object, object>? IconNameToColor { get; set; }
+  public static Func<string, string> ConvertIconName { get; set; } = (iconName) =>
+    string.Concat(iconName.Select((c, i) => char.IsUpper(c) ? i == 0 ? c.ToString().ToLower() : "_" + char.ToLower(c) : c.ToString()));
 
   public static Drawable? GetIcon(Context? context, string? iconName, Dictionary<object, object>? iconNameToColor = null) {
     if (context == null || context.Resources == null || iconName == null) return null;
-    var id = context.Resources.GetIdentifier(iconName.ToLower(), "drawable", context.PackageName);
+    var id = context.Resources.GetIdentifier(ConvertIconName(iconName), "drawable", context.PackageName);
     if (id == 0) return null;
     if (ContextCompat.GetDrawable(context, id) is not { } drawable) return null;
     drawable.Mutate();
