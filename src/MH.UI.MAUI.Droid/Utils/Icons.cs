7b915ec374700b84bc81e9ea6df8b50fbdfe9b45
martin-holy <martin.holy@hotmail.com>
2025-06-06 10:57:58 +0200

    droid utils: icons

--- src/MH.UI.MAUI.Droid/Utils/Icons.cs
+++ src/MH.UI.MAUI.Droid/Utils/Icons.cs
@@ -8,29 +8,24 @@ namespace MH.UI.MAUI.Droid.Utils;
 
 public static class Icons {
   public static Color DefaultColor { get; set; } = Color.Gray;
-  public static Dictionary<string, string>? IconNameToColor { get; set; }
+  public static Dictionary<object, object>? IconNameToColor { get; set; }
 
-  public static Drawable? GetIcon(Context? context, string? iconName, Dictionary<string, string>? iconNameToColor = null) {
+  public static Drawable? GetIcon(Context? context, string? iconName, Dictionary<object, object>? iconNameToColor = null) {
     if (context == null || context.Resources == null || iconName == null) return null;
     var id = context.Resources.GetIdentifier(iconName.ToLower(), "drawable", context.PackageName);
     if (id == 0) return null;
     if (ContextCompat.GetDrawable(context, id) is not { } drawable) return null;
     drawable.Mutate();
-    drawable.SetTint(GetColor(iconName, iconNameToColor ?? IconNameToColor));
+    drawable.SetTint(GetColor(context, iconName, iconNameToColor ?? IconNameToColor));
     return drawable;
   }
 
-  public static Color GetColor(string iconName, Dictionary<string, string>? iconNameToColor) {
-    if (iconNameToColor == null) return DefaultColor;
-    if (!iconNameToColor.TryGetValue(iconName, out var color))
-      iconNameToColor.TryGetValue("default", out color);
-    if (color == null) return DefaultColor;
-
-    try {
-      return Color.ParseColor(color);
-    }
-    catch (System.Exception) {
+  public static int GetColor(Context context, string iconName, Dictionary<object, object>? iconNameToColor) {
+    if (iconNameToColor == null
+      || (!iconNameToColor.TryGetValue(iconName, out var colorId)
+      && !iconNameToColor.TryGetValue("default", out colorId)))
       return DefaultColor;
-    }
+
+    return ContextCompat.GetColor(context, (int)colorId);
   }
 }
