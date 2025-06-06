fef512f46c1c9e079ff177eddf0379486f82c58a
martin-holy <martin.holy@hotmail.com>
2025-06-06 10:53:50 +0200

    sample droid: IconToColorDic

---
+++ src/MH.UI.MAUI.Sample.Droid/Resources/Res.cs
@@ -0,0 +1,19 @@
+﻿using MH.UI.MAUI.Sample.Resources;
+using System.Collections.Generic;
+
+namespace MH.UI.MAUI.Sample.Droid.Resources;
+
+public static class Res {
+  public static readonly Dictionary<object, object> IconToColorDic = new() {
+    { "default", Resource.Color.colorWhite },
+    { Icons.Folder, Resource.Color.colorFolder },
+    { Icons.FolderStar, Resource.Color.colorFolder },
+    { Icons.FolderLock, Resource.Color.colorFolder },
+    { Icons.FolderOpen, Resource.Color.colorFolder },
+    { Icons.Tag, Resource.Color.colorTag },
+    { Icons.TagLabel, Resource.Color.colorTag },
+    { Icons.Drive, Resource.Color.colorDrive },
+    { Icons.DriveError, Resource.Color.colorDrive },
+    { Icons.Cd, Resource.Color.colorDrive }
+  };
+}
