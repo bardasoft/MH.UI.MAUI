1673a19fa62fb6fd5e8e15f1ab43135b3c88e896
martin-holy <martin.holy@hotmail.com>
2025-06-07 18:42:31 +0200

    droid: TreeViewHostViewHolder

--- src/MH.UI.MAUI.Droid/Features/TreeViewHostFt/TreeViewHostViewHolder.cs
+++ src/MH.UI.MAUI.Droid/Features/TreeViewHostFt/TreeViewHostViewHolder.cs
@@ -8,9 +8,9 @@ namespace MH.UI.MAUI.Droid.Features.TreeViewHostFt;
 
 public class TreeViewHostViewHolder(View itemView) : RecyclerView.ViewHolder(itemView) {
   private readonly LinearLayout _container = (LinearLayout)itemView;  
-  private readonly CheckBox _expandCheckBox = itemView.FindViewById<CheckBox>(Resource.Id.expand_checkbox)!;
-  private readonly ImageView _iconImageView = itemView.FindViewById<ImageView>(Resource.Id.icon_image_view)!;
-  private readonly TextView _nameTextView = itemView.FindViewById<TextView>(Resource.Id.name_text_view)!;
+  private readonly ImageView _expandedIcon = itemView.FindViewById<ImageView>(Resource.Id.expanded_icon)!;
+  private readonly ImageView _icon = itemView.FindViewById<ImageView>(Resource.Id.icon)!;
+  private readonly TextView _name = itemView.FindViewById<TextView>(Resource.Id.name)!;
 
   public FlatTreeItem? Item { get; private set; }
 
@@ -21,17 +21,18 @@ public class TreeViewHostViewHolder(View itemView) : RecyclerView.ViewHolder(ite
     int indent = item.Level * _container.Resources?.GetDimensionPixelSize(Resource.Dimension.flat_tree_item_indent_size) ?? 32;
     _container.SetPadding(indent, _container.PaddingTop, _container.PaddingRight, _container.PaddingBottom);
 
-    _expandCheckBox.Checked = item.TreeItem.IsExpanded;
-    _expandCheckBox.CheckedChange -= _onExpandedChanged; // Prevent multiple handlers
-    _expandCheckBox.CheckedChange += _onExpandedChanged;
+    _expandedIcon.Selected = item.TreeItem.IsExpanded;
+    _expandedIcon.Click -= _onExpandedChanged; // Prevent multiple handlers
+    _expandedIcon.Click += _onExpandedChanged;
 
-    _iconImageView.SetImageDrawable(Icons.GetIcon(_container.Context, item.TreeItem.Icon));
+    _icon.SetImageDrawable(Icons.GetIcon(_container.Context, item.TreeItem.Icon));
 
-    _nameTextView.Text = item.TreeItem.Name;
+    _name.SetText(item.TreeItem.Name, TextView.BufferType.Normal);
   }
 
-  private void _onExpandedChanged(object? sender, CompoundButton.CheckedChangeEventArgs e) {
-    if (Item != null)
-      Item.TreeItem.IsExpanded = e.IsChecked;
+  private void _onExpandedChanged(object? sender, System.EventArgs e) {
+    if (Item == null) return;
+    Item.TreeItem.IsExpanded = !Item.TreeItem.IsExpanded;
+    _expandedIcon.Selected = Item.TreeItem.IsExpanded;
   }
 }
