2599c36a20c4c40893a857d74841611268eb41ce
martin-holy <martin.holy@hotmail.com>
2025-06-08 13:27:59 +0200

    CollectionViewHost

---
+++ src/MH.UI.MAUI/Controls/CollectionViewHost.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+using UIC = MH.UI.Controls;
+
+namespace MH.UI.MAUI.Controls;
+
+public class CollectionViewHost : TreeViewHost, UIC.ICollectionViewHost {
+  public static new readonly BindableProperty ViewModelProperty =
+    BindableProperty.Create(nameof(ViewModel), typeof(UIC.CollectionView), typeof(CollectionViewHost), propertyChanged: _onViewModelChanged);
+
+  public new UIC.CollectionView? ViewModel { get => (UIC.CollectionView?)GetValue(ViewModelProperty); set => SetValue(ViewModelProperty, value); }
+  
+  private static void _onViewModelChanged(BindableObject o, object oldValue, object newValue) {
+    if (o is not CollectionViewHost host) return;
+    host.SetValue(TreeViewHost.ViewModelProperty, host.ViewModel);
+    if (host.ViewModel == null) return;
+    host.ViewModel.Host = host;
+    host._onViewModelChanged(oldValue, newValue);
+  }
+}
