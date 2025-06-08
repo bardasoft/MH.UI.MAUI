fcd47affd10468d0761668ec9e9df88d29358de0
martin-holy <martin.holy@hotmail.com>
2025-06-08 13:28:26 +0200

    droid: CollectionViewRowViewHolder

---
+++ src/MH.UI.MAUI.Droid/Features/CollectionViewHostFt/CollectionViewRowViewHolder.cs
@@ -0,0 +1,11 @@
+﻿using Android.Views;
+using AndroidX.RecyclerView.Widget;
+using MH.Utils.BaseClasses;
+
+namespace MH.UI.MAUI.Droid.Features.CollectionViewHostFt;
+
+public class CollectionViewRowViewHolder(View itemView) : RecyclerView.ViewHolder(itemView) {
+  public void Bind(FlatTreeItem? item) {
+
+  }
+}
