56daf22fb20b0a01baa095331f803b7a5cddac63
martin-holy <martin.holy@hotmail.com>
2025-06-08 13:29:13 +0200

    droid: CollectionViewHostHandler

---
+++ src/MH.UI.MAUI.Droid/Features/CollectionViewHostFt/CollectionViewHostHandler.cs
@@ -0,0 +1,41 @@
+﻿using AndroidX.RecyclerView.Widget;
+using MH.UI.MAUI.Controls;
+using Microsoft.Maui;
+using Microsoft.Maui.Handlers;
+
+namespace MH.UI.MAUI.Droid.Features.CollectionViewHostFt;
+
+public class CollectionViewHostHandler : ViewHandler<CollectionViewHost, RecyclerView> {
+  private CollectionViewHostAdapter? _adapter;
+
+  public static IPropertyMapper<CollectionViewHost, CollectionViewHostHandler> PropertyMapper =
+    new PropertyMapper<CollectionViewHost, CollectionViewHostHandler>(ViewMapper) {
+      [nameof(CollectionViewHost.ItemsSource)] = MapItemsSource
+    };
+
+  public static CommandMapper<CollectionViewHost, CollectionViewHostHandler> CommandMapper = new(ViewCommandMapper);
+
+  public CollectionViewHostHandler() : base(PropertyMapper, CommandMapper) { }
+
+  protected override RecyclerView CreatePlatformView() {
+    var recyclerView = new RecyclerView(Context);
+    recyclerView.SetLayoutManager(new LinearLayoutManager(Context));
+    recyclerView.SetBackgroundResource(Resource.Color.c_static_ba); // TODO define this elsewhere
+    return recyclerView;
+  }
+
+  protected override void ConnectHandler(RecyclerView platformView) {
+    base.ConnectHandler(platformView);
+    _adapter = new(Context, VirtualView.ItemsSource);
+    PlatformView.SetAdapter(_adapter);
+  }
+
+  protected override void DisconnectHandler(RecyclerView platformView) {
+    _adapter = null;
+    platformView.SetAdapter(null);
+    base.DisconnectHandler(platformView);
+  }
+
+  private static void MapItemsSource(CollectionViewHostHandler handler, CollectionViewHost view) =>
+    handler._adapter?.UpdateItems(view.ItemsSource);
+}
