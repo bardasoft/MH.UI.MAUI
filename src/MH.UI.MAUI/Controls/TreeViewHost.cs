a6e98960ee1ab1ae480abe2e5b7d6b1f1207f3fa
martin-holy <martin.holy@hotmail.com>
2025-06-12 19:30:55 +0200

    TreeViewHost

--- src/MH.UI.MAUI/Controls/TreeViewHost.cs
+++ src/MH.UI.MAUI/Controls/TreeViewHost.cs
@@ -1,13 +1,6 @@
-﻿using MH.Utils;
-using MH.Utils.BaseClasses;
-using MH.Utils.Extensions;
-using MH.Utils.Interfaces;
+﻿using MH.Utils.Interfaces;
 using Microsoft.Maui.Controls;
 using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Linq;
 using UIC = MH.UI.Controls;
 
 namespace MH.UI.MAUI.Controls;
@@ -24,41 +17,9 @@ public class TreeViewHost : ItemsView, UIC.ITreeViewHost {
   private static void _onViewModelChanged(BindableObject o, object oldValue, object newValue) {
     if (o is not TreeViewHost { ViewModel: not null } host) return;
     host.ViewModel.Host = host;
-    host._onViewModelChanged(oldValue, newValue);
   }
 
   public event EventHandler<bool>? HostIsVisibleChangedEvent;
-
-  internal void _onViewModelChanged(object oldValue, object newValue) {
-    _setItemsSource();
-    if (newValue is UIC.TreeView newTreeView)
-      newTreeView.RootHolder.CollectionChanged += _onRootHolderChanged;
-    if (oldValue is UIC.TreeView oldTreeView)
-      oldTreeView.RootHolder.CollectionChanged -= _onRootHolderChanged;
-  }
-
-  private void _setItemsSource() {
-    if (ViewModel == null) return;
-    var newFlatItems = Tree.ToFlatTreeItems(ViewModel.RootHolder);
-    _updateTreeItemSubscriptions(ItemsSource as IEnumerable<FlatTreeItem>, newFlatItems);
-    ItemsSource = newFlatItems;
-  }
-
-  private void _updateTreeItemSubscriptions(IEnumerable<FlatTreeItem>? oldItems, IEnumerable<FlatTreeItem>? newItems) {
-    var o = oldItems?.Except(newItems ?? []).ToArray() ?? [];
-    var n = newItems?.Except(oldItems ?? []).ToArray() ?? [];
-
-    foreach (var item in o)
-      item.TreeItem.PropertyChanged -= _onTreeItemPropertyChanged;
-
-    foreach (var item in n)
-      item.TreeItem.PropertyChanged += _onTreeItemPropertyChanged;
-  }
-
-  private void _onTreeItemPropertyChanged(object? sender, PropertyChangedEventArgs e) {
-    if (e.Is(nameof(TreeItem.IsExpanded)))
-      _setItemsSource();
-  }
 
   public void ExpandRootWhenReady(ITreeItem root) {
     throw new NotImplementedException();
@@ -71,7 +32,4 @@ public class TreeViewHost : ItemsView, UIC.ITreeViewHost {
   public void ScrollToItems(object[] items, bool exactly) {
     throw new NotImplementedException();
   }
-
-  private void _onRootHolderChanged(object? sender, NotifyCollectionChangedEventArgs e) =>
-    _setItemsSource();
 }
